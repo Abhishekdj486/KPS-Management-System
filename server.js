8e9d5a2c7e759918f4f779b7561b462d8fdbecc5
Abhishek D Jagtap <114818279+Abhishekdj486@users.noreply.github.com>
2024-07-01 18:40:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+// server.js
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/data-management');
+
+// Define a schema
+const DataSchema = new mongoose.Schema({
+    invoiceNo: String,
+    unitSerialNo: String,
+    batterySerialNo: String,
+    oldBatterySerialNo: String,
+    model: String,
+    capacity: String,
+    deliveryDate: String,
+    consumerName: String,
+    address: String,
+    contactNo: String,
+    warranty: String,
+    totalAmount: Number,
+    amountPaid: Number,
+    balanceAmount: Number
+});
+
+const Data = mongoose.model('Data', DataSchema);
+
+// Create route
+app.post('/data', async (req, res) => {
+    const data = new Data(req.body);
+    await data.save();
+    res.send(data);
+});
+
+// Read route
+app.get('/data', async (req, res) => {
+    const data = await Data.find(req.query);
+    res.send(data);
+});
+
+// Update route
+app.put('/data/:id', async (req, res) => {
+    const data = await Data.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.send(data);
+});
+
+// Delete route
+app.delete('/data/:id', async (req, res) => {
+    await Data.findByIdAndDelete(req.params.id);
+    res.send({ message: 'Data deleted' });
+});
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
